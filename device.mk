06f6f2042baecc95090a6d5f277dfec5a443b314
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-02-07 12:24:53 +0530

    laurel_sprout: Include MiuiCamera

--- device.mk
+++ device.mk
@@ -102,6 +102,8 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Camera
+$(call inherit-product, vendor/miuicamera/config.mk)
+ 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
